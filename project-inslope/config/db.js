84ed32d95ccc4b135b9c4887be1378d70f831cae
Firmansyah Wicaksono <firmansyahwicaksono30@gmail.com>
2024-08-30 22:22:18 +0700

    Update db.js

--- project-inslope/config/db.js
+++ project-inslope/config/db.js
@@ -1,13 +1,13 @@
 import { Sequelize } from "sequelize";
 
-//modifikasi konfigurasi database jika akan melakukan pengujian local
-const db = new Sequelize("db-project-inslope", "firmansyw30", "@firmansyw30!", {
-  host: "34.34.216.72", // menggunakan container "mysql" jika untuk pengujian local
+//modify database configuration for local or production
+const db = new Sequelize("database-name", "type-a-username", "type-a-password", {
+  host: "mysql", // using docker container "mysql" for local testing with dockercompose & in this implementation using actual "ip address from db sql instance"
   dialect: "mysql",
-  //port: 3306 // hapus komentar jika dibutuhkan untuk pengujian local
+  //port: 3306
 });
 
-// Tes koneksi
+// db connection test
 db.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
